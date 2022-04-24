9e4fd00064a1b15555b3a2bd0095b56d3702d7f9
Makesh Vineeth <makeshvineeth1997@gmail.com>
2022-04-25 00:36:50 +0530

    Add more code

--- FluentPad/MainPage.xaml.cs
+++ FluentPad/MainPage.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.Net;
+using System.Threading.Tasks;
 using Windows.Storage;
 using Windows.UI;
 using Windows.UI.Popups;
@@ -166,6 +168,170 @@ namespace FluentPad
                 var messageBox = new MessageDialog("Oops, an error has occurred: " + ex.Message, "ERROR");
                 _ = messageBox.ShowAsync();
             }
+        }
+
+        private void SelectAllBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(textBoxMain.Text))
+            {
+                textBoxMain.Focus(FocusState.Programmatic);
+                textBoxMain.SelectAll();
+            }
+        }
+
+        private void InsertDateTimeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            textBoxMain.Focus(FocusState.Programmatic);
+            DateTime now = DateTime.Now;
+            string date_time = now.ToString();
+            textBoxMain.Text += " " + date_time;
+            textBoxMain.SelectionStart = textBoxMain.Text.Length;
+            textBoxMain.SelectionLength = 0;
+        }
+
+        private void PasteClipboardBtn_Click(object sender, RoutedEventArgs e)
+        {
+            textBoxMain.Focus(FocusState.Programmatic);
+            textBoxMain.Text += Environment.NewLine;
+            textBoxMain.SelectionStart = textBoxMain.Text.Length;
+            textBoxMain.SelectionLength = 0;
+            textBoxMain.PasteFromClipboard();
+        }
+
+        private void TrimSpaceBtn_Click(object sender, RoutedEventArgs e)
+        {
+            textBoxMain.Text = textBoxMain.Text.Trim();
+            textBoxMain.Focus(FocusState.Programmatic);
+        }
+
+        private async void AboutBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var messageBox = new MessageDialog(@"Developed by Makesh Vineeth
+Version 1.0
+Copyright © 2022
+All Rights Reserved.", "About Notepad");
+
+            await messageBox.ShowAsync();
+        }
+
+        private async void ShorcutsMenuBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var messageBox = new MessageDialog(@"Alt - Show/Hide Menu
+Ctrl + O to open file
+Ctrl + S to save current file
+Ctrl + F to search for text
+Ctrl + H for replacing text
+Ctrl + G to search in Google
+Ctrl + I to insert date/time
+Ctrl + U for Upper Case
+Ctrl + L for Lower Case", "Shortcuts Guide");
+
+            await messageBox.ShowAsync();
+        }
+
+        private async void SaveCurrentBtn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(textBoxMain.Text)) return;
+
+                if (openedFile == null)
+                {
+                    var saveDialog = new Windows.Storage.Pickers.FileSavePicker();
+                    saveDialog.FileTypeChoices.Add("Plain Text", new List<string>() { ".txt" });
+                    saveDialog.SuggestedFileName = "New Text File";
+                    StorageFile file = await saveDialog.PickSaveFileAsync();
+
+                    if (file != null)
+                    {
+                        openedFile = file;
+                    }
+                }
+
+                if (openedFile == null) return;
+                lastSavedText = textBoxMain.Text;
+                ApplicationView view = ApplicationView.GetForCurrentView();
+                view.Title = openedFile.DisplayName;
+                await FileIO.WriteTextAsync(openedFile, textBoxMain.Text);
+            }
+            catch (Exception)
+            {
+                var messageBox = new MessageDialog("Error has occurred", "ERROR");
+                await messageBox.ShowAsync();
+            }
+        }
+
+        private async void SaveNewBtn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(textBoxMain.Text)) return;
+                var saveDialog = new Windows.Storage.Pickers.FileSavePicker();
+                saveDialog.FileTypeChoices.Add("Plain Text", new List<string>() { ".txt" });
+                saveDialog.DefaultFileExtension = ".txt";
+                saveDialog.SuggestedFileName = "New Text File";
+                StorageFile file = await saveDialog.PickSaveFileAsync();
+
+                if (file != null)
+                {
+                    await FileIO.WriteTextAsync(openedFile, textBoxMain.Text);
+                    openedFile = file;
+                    ApplicationView view = ApplicationView.GetForCurrentView();
+                    view.Title = openedFile.DisplayName;
+                    var messageBox = new MessageDialog("Saved!", "Success");
+                    await messageBox.ShowAsync();
+                }
+            }
+            catch (Exception)
+            {
+                var messageBox = new MessageDialog("Error has occurred", "ERROR");
+                await messageBox.ShowAsync();
+            }
+        }
+
+        private async void SearchTextBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string text = textBoxMain.Text;
+            if (string.IsNullOrWhiteSpace(text)) return;
+            text = text.ToLower();
+
+            string val = await ShowAddDialogAsync("Search for any text");
+            if (string.IsNullOrWhiteSpace(val)) return;
+            previousSearched = val;
+            val = val.ToLower();
+            textBoxMain.Focus(FocusState.Programmatic);
+
+            if (text.Contains(val))
+            {
+                int indexStart = text.IndexOf(val);
+                textBoxMain.SelectionStart = indexStart;
+                textBoxMain.SelectionLength = val.Length;
+            }
+            else
+            {
+                textBoxMain.SelectionLength = 0;
+                textBoxMain.SelectionStart = 0;
+                var messageBox = new MessageDialog("Not found!", "Unable to find");
+                await messageBox.ShowAsync();
+            }
+        }
+
+        public static async Task<string> ShowAddDialogAsync(string title)
+        {
+            var inputTextBox = new TextBox { AcceptsReturn = false };
+            (inputTextBox as FrameworkElement).VerticalAlignment = VerticalAlignment.Bottom;
+            var dialog = new ContentDialog
+            {
+                Content = inputTextBox,
+                Title = title,
+                IsSecondaryButtonEnabled = true,
+                PrimaryButtonText = "Ok",
+                SecondaryButtonText = "Cancel"
+            };
+            if (await dialog.ShowAsync() == ContentDialogResult.Primary)
+                return inputTextBox.Text;
+            else
+                return "";
         }
     }
 }
