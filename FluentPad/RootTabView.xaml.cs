9697a3382e857aac3b472455e4c746ae651d762c
Makesh Vineeth <makeshvineeth1997@gmail.com>
2022-12-08 20:28:04 +0530

    Initial implementation of tabs

---
+++ FluentPad/RootTabView.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Core;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using muxc = Microsoft.UI.Xaml.Controls;
+
+
+namespace FluentPad
+{
+    public sealed partial class RootTabView : Page
+    {
+        public RootTabView()
+        {
+            this.InitializeComponent();
+
+            var coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
+            coreTitleBar.ExtendViewIntoTitleBar = true;
+            coreTitleBar.LayoutMetricsChanged += CoreTitleBar_LayoutMetricsChanged;
+
+            Window.Current.SetTitleBar(CustomDragRegion);
+        }
+
+        private void CoreTitleBar_LayoutMetricsChanged(CoreApplicationViewTitleBar sender, object args)
+        {
+            if (FlowDirection == FlowDirection.LeftToRight)
+            {
+                CustomDragRegion.MinWidth = sender.SystemOverlayRightInset;
+                ShellTitlebarInset.MinWidth = sender.SystemOverlayLeftInset;
+            }
+            else
+            {
+                CustomDragRegion.MinWidth = sender.SystemOverlayLeftInset;
+                ShellTitlebarInset.MinWidth = sender.SystemOverlayRightInset;
+            }
+
+            CustomDragRegion.Height = ShellTitlebarInset.Height = sender.Height;
+        }
+
+        private void TabView_AddTabButtonClick(muxc.TabView sender, object args)
+        {
+            var newTab = new muxc.TabViewItem
+            {
+                IconSource = new muxc.SymbolIconSource() { Symbol = Symbol.Document },
+                Header = "New Document"
+            };
+            Frame frame = new Frame();
+            newTab.Content = frame;
+            frame.Navigate(typeof(MainPage));
+
+            sender.TabItems.Add(newTab);
+            tabView.SelectedIndex++;
+        }
+
+        private void TabView_TabCloseRequested(muxc.TabView sender, muxc.TabViewTabCloseRequestedEventArgs args)
+        {
+            sender.TabItems.Remove(args.Tab);
+        }
+    }
+}
